4802d2e80c06bd974674273fe63c3f1bbaf9214a
jas- <jason.gerfen@gmail.com>
2012-07-25 07:54:32 -0600

    Wed Jul 25 07:54:32 MDT 2012 - Modifications to Session_Destroy() stored procedure which was causing destruction of all current sessions when new sessions were created through session handler

--- install/stored-procedures/sp_sessions.sql
+++ install/stored-procedures/sp_sessions.sql
@@ -2,44 +2,38 @@ DELIMITER //
 
 -- Retrieve current session
 DROP PROCEDURE IF EXISTS Session_Search//
-CREATE DEFINER='[dbUser]'@'[dbHost]' PROCEDURE Session_Search(IN `session_id` VARCHAR(128), IN `sKey` LONGTEXT)
+CREATE DEFINER='[dbUser]'@'[dbHost]' PROCEDURE Session_Search(IN `sessionID` VARCHAR(128), IN `sKey` LONGTEXT)
  DETERMINISTIC
  SQL SECURITY INVOKER
  COMMENT 'Retrieve current session'
 BEGIN
- SELECT `session_id`,AES_DECRYPT(BINARY(UNHEX(session_data)), SHA1(sKey)) AS session_data,`session_expire`,`session_agent`,`session_ip`,`session_referer` FROM `sessions` WHERE `session_id`=session_id;
+ SELECT `session_id`,AES_DECRYPT(BINARY(UNHEX(session_data)), SHA1(sKey)) AS session_data,`session_expire`,`session_agent`,`session_ip`,`session_referer` FROM `sessions` WHERE `session_id`=sessionID;
 END//
 
 DROP PROCEDURE IF EXISTS Session_Add//
-CREATE DEFINER='[dbUser]'@'[dbHost]' PROCEDURE Session_Add(IN `session_id` VARCHAR(64), `session_data` LONGTEXT, `session_expire` INT(10), `session_agent` VARCHAR(64), `session_ip` VARCHAR(64), `session_referer` VARCHAR(64), IN `sKey` LONGTEXT)
+CREATE DEFINER='[dbUser]'@'[dbHost]' PROCEDURE Session_Add(IN `sessionID` VARCHAR(64), `sessionData` LONGTEXT, `sessionExpire` INT(10), `sessionAgent` VARCHAR(64), `sessionIP` VARCHAR(64), `sessionReferer` VARCHAR(64), IN `sKey` LONGTEXT)
  DETERMINISTIC
  SQL SECURITY INVOKER
  COMMENT 'Add or update existing session id & data'
 BEGIN
  DECLARE x INT DEFAULT 0;
- DECLARE sdata VARCHAR(255);
- SELECT COUNT(*) INTO x FROM `sessions` WHERE `session_id`=session_id;
+ SELECT COUNT(*) INTO x FROM `sessions` WHERE `session_id`=sessionID AND session_agent=sessionAgent AND session_ip=sessionIP;
  IF x > 0
  THEN
-  SELECT AES_DECRYPT(BINARY(UNHEX(session_data)), SHA1(sKey)) INTO sdata FROM `sessions` WHERE `session_id`=session_id;
-  SELECT STRCMP(sdata, session_data);
-  IF (STRCMP(sdata, session_data) = 0)
-  THEN
-   UPDATE `sessions` SET `session_data`=HEX(AES_ENCRYPT(CONCAT(sdata, session_data), SHA1(sKey))) WHERE `session_id`=session_id LIMIT 1;
-  END IF;
+  UPDATE `sessions` SET `session_data`=HEX(AES_ENCRYPT(sessionData, SHA1(sKey))) WHERE `session_id`=sessionID LIMIT 1;
  ELSE
-  INSERT INTO `sessions` (`session_id`,`session_data`,`session_expire`,`session_agent`,`session_ip`,`session_referer`) VALUES (session_id, HEX(AES_ENCRYPT(session_data, SHA1(sKey))), session_expire, session_agent, session_ip, session_referer) ON DUPLICATE KEY UPDATE `session_id`=session_id, `session_data`=HEX(AES_ENCRYPT(session_data, SHA1(sKey))), `session_expire`=session_expire;
+  INSERT INTO `sessions` (`session_id`,`session_data`,`session_expire`,`session_agent`,`session_ip`,`session_referer`) VALUES (sessionID, HEX(AES_ENCRYPT(sessionData, SHA1(sKey))), sessionExpire, sessionAgent, sessionIP, sessionReferer) ON DUPLICATE KEY UPDATE `session_id`=sessionID, `session_data`=HEX(AES_ENCRYPT(sessionData, SHA1(sKey))), `session_expire`=sessionExpire;
  END IF;
  SELECT ROW_COUNT() AS affected;
 END//
 
 DROP PROCEDURE IF EXISTS Session_Destroy//
-CREATE DEFINER='[dbUser]'@'[dbHost]' PROCEDURE Session_Destroy(IN `session_id` VARCHAR(64))
+CREATE DEFINER='[dbUser]'@'[dbHost]' PROCEDURE Session_Destroy(IN `sessionID` VARCHAR(64))
  DETERMINISTIC
  SQL SECURITY INVOKER
  COMMENT 'Delete users sessions id'
 BEGIN
- DELETE FROM `sessions` WHERE `session_id`=session_id LIMIT 1;
+ DELETE FROM `sessions` WHERE `session_id`=sessionID LIMIT 1;
 END//
 
 DROP PROCEDURE IF EXISTS Session_Timeout//
